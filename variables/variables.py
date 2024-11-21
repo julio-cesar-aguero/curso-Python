51944ace3dd6efd0c5aa672e01f426585683a376
juliocesar.aguero.contractor <juliocesar.aguero.contractor@bbva.com>
2024-11-20 22:05:19 -0600

    Variables y datos simples

--- variables/variables.py
+++ variables/variables.py
@@ -3,5 +3,21 @@ b = 35
 c = a + b
 print(c)
 
+#Definicion de variables
+
 Nombre = "Ian Aran"
+#Concatenar datos
+message = 'Hola '+Nombre
+message2= f"{message}"
 print(Nombre)
+print(message)
+print(message2)
+#operadores de pertenencia
+print("Ian" in message2) #true
+print("Aguero" in message2) 
+print("Ian" not in message2)
+print("Aguero" not in message2) #true
+
+#snake case
+variable_con_snake_case = "snake_case"
+print(variable_con_snake_case) #snake_case
